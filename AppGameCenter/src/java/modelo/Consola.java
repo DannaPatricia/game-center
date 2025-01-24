040d8ae854d3c527617c8cb0ce5aa89a1bbdf43f
Danna Patricia <dannapatriciacm@gmail.com>
2025-01-24 22:01:40 +0100

    Update Consola.java

--- AppGameCenter/src/java/modelo/Consola.java
+++ AppGameCenter/src/java/modelo/Consola.java
@@ -2,26 +2,33 @@ package modelo;
 
 import dao.ConsolaDAO;
 
+// La clase Consola extiende de Producto, lo que sugiere que es un tipo especializado de Producto.
 public class Consola extends Producto {
 
-    private double potenciaCpu;
-    private double potenciaGpu;
-    private String compania;
+    // Atributos específicos de la consola
+    private double potenciaCpu;  // Potencia del procesador en GHz
+    private double potenciaGpu;  // Potencia de la tarjeta gráfica en TFLOPS
+    private String compania;     // Compañía fabricante de la consola (Sony, Microsoft, etc.)
 
+    // Constructor vacío, llama al constructor de la clase base Producto.
     public Consola() {
         super();
     }
 
+    // Constructor que solo inicializa el ID, llamando al constructor de la clase base.
     public Consola(int id) {
         super(id);
     }
 
+    // Constructor que inicializa todos los atributos (incluidos los heredados de Producto)
     public Consola(int id, String nombre, double precio, int unidadesDisponibles, double potenciaCpu, double potenciaGpu, String compania) {
-        super(id, nombre, precio, unidadesDisponibles);
+        super(id, nombre, precio, unidadesDisponibles);  // Inicializa los atributos de Producto
         this.potenciaCpu = potenciaCpu;
         this.potenciaGpu = potenciaGpu;
         this.compania = compania;
     }
+
+    // Getters y setters para los atributos específicos de la consola
 
     public double getPotenciaCpu() {
         return potenciaCpu;
@@ -47,12 +54,15 @@ public class Consola extends Producto {
         this.compania = compania;
     }
 
+    // Método estático que genera un elemento <select> con las opciones de consolas disponibles.
     public static String generaOpcionMultiple(String consola) {
+        // Llama a un método de ConsolaDAO para generar las opciones basadas en las consolas disponibles en la base de datos
         return "<select id=\"nombre\" name=\"nombre\">"
                 + ConsolaDAO.generaOpcionMultiple(consola)
                 + "        </select>\n";
     }
 
+    // Muestra un botón para ver detalles del producto, con el id del producto como valor del botón.
     public String muestraBtnConId() {
         return "<div class=\"elementoContainer\">"
                 + "<img src=\"imagenesConsolas/xboxOne.jpg\" alt=\"xboxOne\">"
@@ -61,6 +71,7 @@ public class Consola extends Producto {
                 + "</div>";
     }
 
+    // Método que genera el HTML con la información básica de la consola para mostrarla al cliente.
     @Override
     public String mostrarInfoParcial() {
         return "<div class=\"elementoContainer\">"
@@ -72,6 +83,7 @@ public class Consola extends Producto {
                 + "</div>";
     }
 
+    // Método utilizado para mostrar la consola en la interfaz de administración, permitiendo realizar acciones sobre ella (modificar, eliminar, etc.)
     @Override
     public String mostrarParaAdmin(String opcion) {
         return "<div class=\"contenedorAdmin\">"
@@ -84,6 +96,7 @@ public class Consola extends Producto {
                 + "</div>";
     }
 
+    // Genera la información completa de la consola, con detalles como la potencia de la CPU, GPU, y el precio
     @Override
     public String mostrarInformacion() {
         return "<div class=\"elementoContainer\">"
@@ -100,16 +113,18 @@ public class Consola extends Producto {
                 + "</div>";
     }
 
+    // Método para simular la compra de un producto, descontando unidades disponibles.
     @Override
     public void comprarProducto() {
         if (unidadesDisponibles > 0) {
-            unidadesDisponibles--;
+            unidadesDisponibles--;  // Disminuye las unidades disponibles
             System.out.println("Compra realizada de " + nombre + ". Unidades disponibles ahora: " + unidadesDisponibles);
         } else {
-            System.out.println("No hay unidades disponibles de " + nombre);
+            System.out.println("No hay unidades disponibles de " + nombre);  // Si no hay unidades disponibles, muestra un mensaje.
         }
     }
 
+    // Genera el formulario HTML para agregar una nueva consola, con campos como nombre, potencia, precio, etc.
     public static String generaContenidoFormulario() {
         StringBuilder html = new StringBuilder();
         html.append("  <div class=\"input-group\">\n"
@@ -139,6 +154,6 @@ public class Consola extends Producto {
                 + "        <div class=\"input-group\">\n"
                 + "            <button type=\"submit\" class=\"btn btn-primary\" name = \"opcionEnviada\" value = \"consola\">Insertar</button>"
                 + "        </div>");
-        return html.toString();
+        return html.toString();  // Devuelve el formulario HTML generado
     }
 }
